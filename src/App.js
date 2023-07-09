4d8c56e88d85baea25ae8922fa5fe505d2c1636c
sean-kintone <seanluse@cybozu.onmicrosoft.com>
2023-07-09 22:30:12 +0900

    country state city picker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
 import './App.css';
+import { useEffect, useState } from "react";
+import { Country, State, City } from "country-state-city";
+import Select from 'react-select'
 
 function App() {
+
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedState, setSelectedState] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+  useEffect(() => {
+    console.log(selectedCountry);
+    console.log(selectedCountry?.isoCode);
+    console.log(State?.getStatesOfCountry(selectedCountry?.isoCode));
+  }, [selectedCountry]);
   return (
     <div className="main">
       Welcome to React and Kintone!
-      <div className="locationBox">
-        
-      </div>
+      <Select
+        options={Country.getAllCountries()}
+        getOptionLabel={(options) => {
+          return options["name"];
+        }}
+        getOptionValue={(options) => {
+          return options["name"];
+        }}
+        value={selectedCountry}
+        onChange={(item) => {
+          setSelectedCountry(item);
+        }}
+      />
+      <Select
+        options={State?.getStatesOfCountry(selectedCountry?.isoCode)}
+        getOptionLabel={(options) => {
+          return options["name"];
+        }}
+        getOptionValue={(options) => {
+          return options["name"];
+        }}
+        value={selectedState}
+        onChange={(item) => {
+          setSelectedState(item);
+        }}
+      />
+      <Select
+        options={City.getCitiesOfState(
+          selectedState?.countryCode,
+          selectedState?.isoCode
+        )}
+        getOptionLabel={(options) => {
+          return options["name"];
+        }}
+        getOptionValue={(options) => {
+          return options["name"];
+        }}
+        value={selectedCity}
+        onChange={(item) => {
+          setSelectedCity(item);
+        }}
+      />
     </div>
   );
 }
